f588c81d8c1759d6a5420737df81bfaec25ba448
Almasemilius <almasemilius@gmail.com>
2023-06-15 14:59:23 -0700

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n",
 			sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n",
-			sizeof(long));
+			sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n",
-			sizeof(long));
+			sizeof(long long int));
 	printf("Size of an float: %zu byte(s)\n",
 			sizeof(float));
 	return (0);
