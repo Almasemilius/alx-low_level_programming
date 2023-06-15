31489f3fbf1027769163c9ddb28cbb9965ad8d82
Almasemilius <almasemilius@gmail.com>
2023-06-15 14:22:59 -0700

    main function to print string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-int main()
+
+/**
+ * main - Main Function
+ *
+ * Description: 'Function that prints string and returns zer'
+ *
+ * Return: Returns 0 after execution
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
