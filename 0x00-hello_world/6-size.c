8d4b2d8ae605c97caaaa707b655f5f95076dad7d
Almasemilius <almasemilius@gmail.com>
2023-06-15 14:49:30 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'program prints size of different data types'
+ * Return: Return integer value 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n",
+			sizeof(char));
+	printf("Size of an int: %zu byte(s)\n",
+			sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n",
+			sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n",
+			sizeof(long));
+	printf("Size of an float: %zu byte(s)\n",
+			sizeof(float));
+	return (0);
+}
