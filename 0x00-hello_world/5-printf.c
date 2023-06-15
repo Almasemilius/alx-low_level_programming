ef4c1dfc7cf0092549f6e06cf4307e463ea0714a
Almasemilius <almasemilius@gmail.com>
2023-06-15 14:29:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Main function to print string
+ *
+ * Description: 'C App with one function which prints string using printf '
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
