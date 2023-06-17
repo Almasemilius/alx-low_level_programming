5dbb297bee5033d8779201bc5935287cf52c1770
Almasemilius <almasemilius@gmail.com>
2023-06-17 07:24:27 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: 'The function to print characters'
+ * Return: Returns zero int
+ */
+
+int main(void)
+{
+	char init = 'a';
+
+	for (init = 'a'; init < 'z'; init++)
+		putchar(init);
+	return (0);
+}
