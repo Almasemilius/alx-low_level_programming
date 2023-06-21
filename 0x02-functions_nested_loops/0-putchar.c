6cadc78637817faff4c2e5c8c7556cc53b4804ae
Almasemilius <almasemilius@gmail.com>
2023-06-21 06:36:26 -0700

    c program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *Description: 'Main function to print putchar'
+ *Return: Integer Value 0
+ */
+
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+}
