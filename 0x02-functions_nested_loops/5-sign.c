9ddfbd155cdc79b5b0647f2b79176e7c495a1166
Almasemilius <almasemilius@gmail.com>
2023-06-21 17:27:39 -0700

    loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *print_sign- Entry point
+ *Description: 'Main function to print putchar'
+ *Return: Integer Value 0
+ *@n: Input as parameter
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	} else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
