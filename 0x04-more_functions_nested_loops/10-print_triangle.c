0ee82b365497469456974b6974a8d51ed6856eda
Almasemilius <almasemilius@gmail.com>
2023-06-23 17:14:53 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *print_triangle - Entry point
+ *Description: 'Main function to print putchar'
+ *@size: parameter
+ *Return: Integer Value 0
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
