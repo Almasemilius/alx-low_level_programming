848ade4c79d10422032341b9ed6098ce8b400351
Almas <almasemilius@gmail.com>
2023-06-27 00:36:37 +0300

    question 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code
+ * Description: 'Change pointer value'
+ * @n: Pointer to variable n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _PUTCHAR_H_
+#define _PUTCHAR_H_
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+void rev_string(char *s);
+
+#endif
