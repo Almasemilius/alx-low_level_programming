c21fe3a8d6ee8400c990247adb4c37f5c0514704
Almas <almasemilius@gmail.com>
2023-06-30 00:29:50 +0300

    q3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,31 +9,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 != '\0' && *s1 == *s2)
 	{
-		if (*s1 < *s2)
-		{
-			return (-15);
-		}
-		else if (*s1 > *s2)
-		{
-			return (15);
-		}
-
 		s1++;
 		s2++;
 	}
 
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (0);
-	}
-	else if (*s1 == '\0')
-	{
-		return (-1);
-	}
-	else
-	{
-		return (1);
-	}
+	return (*s1 - *s2);
 }
