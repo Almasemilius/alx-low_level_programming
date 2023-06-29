5a7187adb5a5844ac53f2ce02500073a48907b8e
Almas <almasemilius@gmail.com>
2023-06-29 23:27:10 +0300

    qn 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - check the code
+ * Description: 'Change pointer value'
+ * @s1: Pointer to variable str
+ * @s2: Pointer variable
+ * Return: Always Integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 < *s2)
+		{
+			return (-15);
+		}
+		else if (*s1 > *s2)
+		{
+			return (15);
+		}
+
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
+	else if (*s1 == '\0')
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
