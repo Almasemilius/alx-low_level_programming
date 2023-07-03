330d97493ecd93590b11c3ae575c2b33d6fb4595
Almas <almasemilius@gmail.com>
2023-07-03 16:27:03 +0300

    qn 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * set_string - check the code
+ * Description: 'Change pointer value'
+ * @s: Pointer to variable str
+ * @to: Pointer variable
+ * Return: Character
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
