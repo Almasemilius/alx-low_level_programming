b98175052b86a82b94d3cfae41390f305eba2ced
Almas <almasemilius@gmail.com>
2023-07-05 23:33:44 +0300

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - check the code
+ * Description: 'Recursion function'
+ * @s: Pointer to variable str
+ * Return: Always null
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	printf("%c", *s);
+}
