02b9fe9e35e4f42955d1ceee354b3e8254448a77
Almas <almasemilius@gmail.com>
2023-07-08 00:13:14 +0300

    q  and 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: 'Recursion function'
+ * @argc: Pointer to variable str
+ * @argv: Pointer to variable str
+ * Return: Always null
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
