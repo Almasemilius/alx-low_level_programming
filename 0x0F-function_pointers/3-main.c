0046e8940e8e3b7230be74f845741ef9bcc07874
Almas <almasemilius@gmail.com>
2023-07-19 23:05:42 +0300

    changes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * main - creating main function
- *@ac: int
- *@argv: arguments
- *Return: 0.
+ * main - Entry point
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 on success, 98 if the number of arguments is wrong,
+ *         99 if the operator is invalid, and 100 if there's an attempt
+ *         to divide or modulo by zero.
  */
-
-int main(int ac, char *argv[])
+int main(int argc, char *argv[])
 {
-	int x;
-	int y;
-	int (*a)(int, int);
+	int num1;
+	int result;
+	char *operator;
+	int num2;
+	int (*operation)(int, int);
 
-	if (ac != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
-	if (argv[2][1] != '\0')
+
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(operator);
+
+	if (operation == NULL)
 	{
 		printf("Error\n");
-		exit(99);
+		return (99);
 	}
 
-	a = get_op_func(argv[2]);
-
-	if (a == NULL)
+	if ((*operator == '/' || *operator == '%') && num2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		return (100);
 	}
 
-	x = atoi(argv[1]);
-	y = atoi(argv[3]);
-
-	printf("%d\n", a(x, y));
+	result = operation(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
