0d8a7422bd3f0c25f25b1526553f3ebfcc3c4513
Almas <almasemilius@gmail.com>
2023-07-19 23:08:51 +0300

    Revert "q 3"
    This reverts commit 61ed850ee21f0d781ed9d764289abec155912ac7.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,30 @@
 #include "3-calc.h"
-#include "function_pointers.h"
+#include <stddef.h>
 
 /**
- * get_op_func - select function to perform operation
- *@s: struct
- * Return: NULL
+ * get_op_func - Selects the correct function to perform the operation
+ *               asked by the user.
+ * @s: The operator passed as an argument to the program.
+ *
+ * Return: A pointer to the function that corresponds to the operator
+ *         given as a parameter. If the operator is not found, return NULL.
  */
-
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+	    {"+", op_add},
+	    {"-", op_sub},
+	    {"*", op_mul},
+	    {"/", op_div},
+	    {"%", op_mod},
+	    {NULL, NULL}};
 
-	i = 0;
-	while (i < 5)
+	int i = 0;
+
+	while (ops[i].op != NULL)
 	{
-		if (ops[i].op[0] == s[0])
-		{
+		if (*(ops[i].op) == *s)
 			return (ops[i].f);
-		}
 		i++;
 	}
 	return (NULL);
