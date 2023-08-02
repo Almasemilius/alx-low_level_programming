b3dc0a126fb29b3ec9aeb4e39d28d985d365f096
Almas <almasemilius@gmail.com>
2023-08-03 02:52:50 +0300

    singly

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len -  function that returns the number of elements in a linked
+ * @h: pointer to linked list
+ *
+ * Return: number
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
