282f7372fdf9973f28a97b40866827a6d7092f80
Almas <almasemilius@gmail.com>
2023-08-03 23:32:59 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,14 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
-	{
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	}
 
-	*n = *n - (1ul << index);
+	unsigned long int mask = 1UL << index;
+
+	mask = ~mask;
+
+	*n = *n & mask;
 
 	return (1);
 }
