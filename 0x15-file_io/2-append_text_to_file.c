79c5be664cb1cbbbd38975630a8b02997137d53f
Almas <almasemilius@gmail.com>
2023-08-08 01:15:11 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	length = 0;
 
 	if (filename == NULL)
+	{
 		return (-1);
+	}
 
 	if (text_content != NULL)
 	{
@@ -26,7 +28,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	b = write(a, text_content, length);
 
 	if (a == -1 || b == -1)
+	{
 		return (-1);
+	}
 
 	close(a);
 
